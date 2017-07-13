eacdefd04389abcb383ba3ea5479ebf279a9fb4e
Pratik Basak <pratikb@optimizeitsystems.com>
2017-07-13 20:04:11 +0530

    restrict syntax fix for directives

--- app/application/shared/header/header.directive.js
+++ app/application/shared/header/header.directive.js
@@ -2,7 +2,7 @@
 
 angular.module('movieApp.header.directive',[]).directive('movieNavbar', function(){
     return {
-        restrict: EA,
+        restrict: 'EA',
         templateUrl: 'application/shared/header/header.html',
         link: function (scope, elem, attrs, state) {
             // ..
