fb30037332f77df694cb033a945cc8a40b67a934
Pratik Basak <pratikb@optimizeitsystems.com>
2017-07-14 16:06:47 +0530

    movies listing view

--- app/application/components/movies/movies.controller.js
+++ app/application/components/movies/movies.controller.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module('movieApp.movies.controller', []).controller('MoviesController',
-['$scope', '$log', function($scope, $log){
+['$scope', '$log', 'MoviesService', function($scope, $log, MoviesService){
     $log.log('Movies Controller.. ');
+
+    (function(){
+        MoviesService.getData().then(function(dataparam){
+            $log.log('dataparam->'+dataparam);
+            $scope.movielist = dataparam;
+        })
+    })();
+
 }]);
--- app/application/components/movies/movies.service.js
+++ app/application/components/movies/movies.service.js
@@ -1,8 +1,21 @@
 'use strict';
 
-angular.module('movieApp.movies.service', []).factory('MoviesService',
-['$scope', '$log', function($scope, $log){
-    return {
+angular.module('movieApp.movies.service', []).service('MoviesService',
+['$scope', '$log', '$q', function($scope, $log, $q){
 
-    };
+    var data;
+        
+    this.getData = function(){
+        var def = $q.defer();
+        $http.get('http://starlord.hackerearth.com/movieslisting').then(function callSuccess(response){
+            $log.log('response->'+response);
+            data = response;
+            def.resolve('Data resolved ..'+data);
+        }, function callError(error){
+            $log.error('Error fetching data ..!');
+            def.reject('Error fetching data ..');
+        });
+        return def.promise;
+    }
+
 }]);
