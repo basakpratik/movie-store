288c1fad969569c7990445fb6d4db4683c4d654b
Pratik Basak <basak.pratik@outlook.com>
2017-08-23 23:17:28 +0530

    sorting in movies list page ..

--- app/application/shared/sort/sort.controller.js
+++ app/application/shared/sort/sort.controller.js
@@ -5,32 +5,32 @@ angular.module('movieApp.sort.controller',[]).controller('SortController',
 function($scope, $log, $state, $rootScope, MoviesService){
     $log.log('Sort Controller ..');
 
-    $scope.getmovielist = MoviesService.msGetFilteredMovies();
-    $scope.getMovieListLength = $scope.getmovielist.length;
-
-    /* for(let i=0; i<$scope.getMovieListLength; i++){
-        console.log($scope.getmovielist[i].title_year);
-    } */
-    $scope.getmovielist.sort(function(a, b){
-        return a.title_year - b.title_year;
-    })
-    console.dir($scope.getmovielist);
+    let getmovielist = MoviesService.msGetFilteredMovies();
 
     $scope.sortByFunc = function(sortBy){
         //$log.log('sortType: '+$scope.sortType);
         if(sortBy == 'year'){
             if($scope.sortType == 'Oldest-to-Latest'){
-                // ..
+                getmovielist.sort(function(a, b){
+                    return a.title_year - b.title_year;
+                });
             } else if($scope.sortType == 'Latest-to-Oldest'){
-                // ..
+                getmovielist.sort(function(a, b){
+                    return b.title_year - a.title_year;
+                });
             }
         } else if(sortBy == 'budget'){
             if($scope.sortType == 'Low-to-High'){
-                // ..
+                getmovielist.sort(function(a, b){
+                    return a.budget - b.budget;
+                });
             } else if($scope.sortType == 'High-to-Low'){
-                // ..
+                getmovielist.sort(function(a, b){
+                    return b.budget - a.budget;
+                });
             }
         }
-        
+        MoviesService.msSetFilteredMovies(getmovielist);
+        $state.go('movies', null, {'reload':true});        
     }
 }]);
